78c73fbf1b8988ade73e4191200df4eba792d208
Larry Pham <phipv@evolableasia.vn>
2014-08-13 08:54:23 +0700

    Creating Bookie Project

---
+++ src/com/techiedb/app/BookieApp/BookieAppActivity.java
@@ -0,0 +1,15 @@
+package com.techiedb.app.BookieApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BookieAppActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
